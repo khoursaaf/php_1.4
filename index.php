3d0361dabc4fd1407baad8b45956217ba9361dd8
khoursaaf <khoursa@gmail.com>
2018-11-19 10:02:57 +0100

    fist commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Créer une variable de type string, une variable de type int, une variable de type float, une variable de type booléan et les initialiser avec une valeur de votre choix. Les afficher.</p>
+      <p><?php $string = "Variable type text"; echo $string; ?></p>
+      <p><?php $int = 189; echo $int; ?></p>
+      <p><?php $float = 1.89; echo $float; ?></p>
+      <p><?php $bol = true; echo $bol; ?></p>
+    </body>
+</html>
