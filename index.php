7f042dc9f533e4f360c0ea2d18a61a5d281031c5
khoursaaf <khoursa@gmail.com>
2018-11-20 16:19:35 +0100

    correction

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Créer une variable de type string, une variable de type int, une variable de type float, une variable de type booléan et les initialiser avec une valeur de votre choix. Les afficher.</p>
-      <p><?php $string = "Variable type text"; echo $string; ?></p>
-      <p><?php $int = 189; echo $int; ?></p>
-      <p><?php $float = 1.89; echo $float; ?></p>
-      <p><?php $bol = true; echo $bol; ?></p>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer une variable de type string, une variable de type int, une variable de type float, une variable de type booléan et les initialiser avec une valeur de votre choix. Les afficher.</p>
+    <?php $string = "Variable type text"; echo $string; ?>
+    <?php $int = 189; echo $int; ?>
+    <?php $float = 1.89; echo $float; ?>
+    <?php $bol = true; echo $bol; ?>
+    <p>Ceci est une variable string : <?php echo $string; ?></p>
+    <p>Ceci est une variable integer : <?php echo $int; ?></p>
+    <p>Ceci est une variable float : <?php echo $float; ?></p>
+    <p>Ceci est une variable booléan : <?php echo $bol; ?></p>
+  </body>
 </html>
